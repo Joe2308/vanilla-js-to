528946b51398c4c54fa7cd0ad2d703298ab38213
Joe <joebreilly@yahoo.ie>
2022-03-16 23:00:12 +0000

    Completed and deleted functionality added. Select drop down added to show: all, completed and uncompleted tasks

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector('.filter-todo')
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // Functions
 function addTodo(e){
@@ -62,6 +64,34 @@ function deleteCheck(e){
     // Delete ToDo
     if(item.classList[0] === 'delete-btn'){
         const todo = item.parentElement;
-        todo.remove();
+        // Animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+        
     }
+
+    // Check mark
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch (e.target.value){
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display = 'block';
+                }else{
+                    todo.style.display = 'block';
+                }
+        }
+    });
 }
