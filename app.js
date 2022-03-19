cdafebbf9eef1187dc5e386ef671d6f042b35860
Joe <joebreilly@yahoo.ie>
2022-03-19 17:35:38 +0000

    Save todos to local storage funcitonality implemented

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const todoList = document.querySelector('.todo-list')
 const filterOption = document.querySelector('.filter-todo')
 
 // Event Listeners
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-filterOption.addEventListener('click', filterTodo);
+filterOption.addEventListener('change', filterTodo);
 
 // Functions
 function addTodo(e){
@@ -31,6 +32,8 @@ function addTodo(e){
 
     // Place each todo li inside the Todo div
     todoDiv.appendChild(newTodo);
+    // Add todo to local storage
+    saveLocalTodos(todoInput.value);
 
     // Create completed button
     const completedButton = document.createElement('button');
@@ -81,17 +84,93 @@ function deleteCheck(e){
 
 function filterTodo(e) {
     const todos = todoList.childNodes;
-    todos.forEach(function(todo){
-        switch (e.target.value){
-            case "all":
-                todo.style.display = 'flex';
-                break;
-            case "completed":
-                if(todo.classList.contains("completed")){
-                    todo.style.display = 'block';
-                }else{
-                    todo.style.display = 'block';
+    todos.forEach(function (todo) {
+                const mStyle = todo.style;
+                if (mStyle != undefined && mStyle != null) {
+                    switch (e.target.value) {
+                        case "all":
+                            mStyle.display = "flex";
+                            break;
+                        case "completed":
+                            if (todo.classList.contains('completed')) {
+                                mStyle.display = 'flex';
+                            } else {
+                                mStyle.display = "none";
+                            }
+                            break;
+                        case "uncompleted":
+                            if (todo.classList.contains('completed')) {
+                                mStyle.display = 'none';
+                            } else {
+                                mStyle.display = "flex";
+                            }
+                            break;
+                    }
                 }
-        }
     });
 }
+
+
+function saveLocalTodos(todo){
+    // Check if an todos already in there
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodos(){
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function(todo){
+        // Create Todo DIV inside ul
+    const todoDiv = document.createElement('div');
+
+    // Add a class to the todo div
+    todoDiv.classList.add('todo');
+
+    // Create list items 
+    const newTodo = document.createElement('li');
+
+    // Add text from the input to the li
+    newTodo.innerText = todo;
+
+    // add a class to the todo li
+    newTodo.classList.add('todo-item');
+
+    // Place each todo li inside the Todo div
+    todoDiv.appendChild(newTodo);
+
+     // Create completed button
+     const completedButton = document.createElement('button');
+ 
+     // Add check icon to button
+     completedButton.innerHTML = '<i class="fas fa-check"></i>';
+     completedButton.classList.add('complete-btn');
+     
+     // Add the button to the Todo div
+     todoDiv.appendChild(completedButton);
+ 
+     // Create delete button
+     const deleteButton = document.createElement('button');
+ 
+     // Add trash icon to button
+     deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+     deleteButton.classList.add('delete-btn');
+     
+     // Add the button to the Todo div
+     todoDiv.appendChild(deleteButton);
+ 
+     // Append to List
+     todoList.appendChild(todoDiv);
+    });
+}
+
